3111a5bf8c680a200151b70390a768cbdcc15d20
Eymen <cimena19@itu.edu.tr>
2021-01-17 04:18:38 +0300

    procfile fix

--- server.py
+++ server.py
@@ -9,23 +9,25 @@ cur = conn.cursor()
 cur.execute("SELECT * FROM player")
 records = cur.fetchall()
 
-def create_app():
-	app = Flask(__name__)
-	app.config.from_object("config")
-	app.add_url_rule("/", view_func=views.home_page)
-	app.add_url_rule("/leaderboard", view_func=views.leaderboard_page, methods=["GET", "POST"])
-	app.add_url_rule("/matches", view_func=views.matches_page)
-	app.add_url_rule("/profile", view_func=views.profile_page, methods=["GET", "POST"])
-	app.add_url_rule(
-		"/profile/<int:movie_key>/edit",
-		view_func=views.movie_edit_page,
-		methods=["GET", "POST"],
-	)
 
-	db = Database()
-	app.config["db"] = db
-	return app
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config")
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/leaderboard", view_func=views.leaderboard_page, methods=["GET", "POST"])
+    app.add_url_rule("/matches", view_func=views.matches_page)
+    app.add_url_rule("/profile", view_func=views.profile_page, methods=["GET", "POST"])
+    app.add_url_rule(
+        "/profile/<int:movie_key>/edit",
+        view_func=views.movie_edit_page,
+        methods=["GET", "POST"],
+    )
+
+    db = Database()
+    app.config["db"] = db
+    return app
+
 
 if __name__ == "__main__":
-	app = create_app()
-	app.run(host="127.0.0.1", port=8080)
+    app = create_app()
+    app.run(host="127.0.0.1", port=8080)
